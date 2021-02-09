3f4eec1527294f0dffe59464cd9228a67a11a70f
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-02-09 18:52:40 +0600

    ok

---
+++ hard-rock-master/app.js
@@ -0,0 +1,45 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    //load data
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('songContainer');
+    songContainer.innerHTML ='';
+    songs.forEach(song => {
+        console.log(song);
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3'
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                <source src="${song.preview}" type="audio/ogg">
+              </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        
+        `;
+        songContainer.appendChild(songDiv);
+
+    });
+}
+
+const getLyric = (artist,title) =>{
+    const url =`https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+    .then(res => res.json())
+    .then (data => DisplayLyrics(data.lyrics))
+}
+
+const DisplayLyrics = lyrics =>{
+    const lyricsDiv = document.getElementById('songLyrics');
+    lyricsDiv.innerText = lyrics;
+}
